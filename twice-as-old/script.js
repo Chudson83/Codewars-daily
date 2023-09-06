5fb1774a9bbea09691ea869cbcac014f1f2cf9fc
Chris Hudson <Chudson83@gmail.com>
2023-09-06 13:28:55 -0500

    Version 2_Cleaner Code

--- twice-as-old/script.js
+++ twice-as-old/script.js
@@ -1,10 +1,3 @@
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-  let sonDbl = sonYearsOld * 2;
-  if (sonDbl < dadYearsOld) {
-    return dadYearsOld - sonDbl;
-  } else if (sonDbl > dadYearsOld) {
-    return sonDbl - dadYearsOld;
-  } else {
-    return 0;
-  }
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
